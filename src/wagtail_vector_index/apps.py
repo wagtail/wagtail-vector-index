299403f652c94501e1dc4eb672dc94d951e42606
Alex Morega <alex.morega@torchbox.com>
2024-01-08 16:18:18 +0200

    Set default auto field

--- src/wagtail_vector_index/apps.py
+++ src/wagtail_vector_index/apps.py
@@ -5,3 +5,4 @@ class WagtailVectorIndexAppConfig(AppConfig):
     label = "wagtail_vector_index"
     name = "wagtail_vector_index"
     verbose_name = "Wagtail Vector Index"
+    default_auto_field = "django.db.models.AutoField"
