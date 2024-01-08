89fef42cb7b48a2eaeff7dc9ff8db7a91cd3f9fc
Alex Morega <alex@grep.ro>
2024-01-08 18:50:25 +0200

    Set default auto field (#32)

--- src/wagtail_vector_index/backends/pgvector/apps.py
+++ src/wagtail_vector_index/backends/pgvector/apps.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class PgvectorConfig(AppConfig):
     name = "wagtail_vector_index.backends.pgvector"
     verbose_name = _("pgvector")
+    default_auto_field = "django.db.models.AutoField"
 
     # TODO: Check if we're using a Postgres database.
     #       Otherwise this app should not be allowed.
