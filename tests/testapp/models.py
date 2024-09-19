59c363dfb5a5b4c3bc4a1f8d5fd516b1f77ac8e1
Tom Usher <tom@tomusher.com>
2024-09-19 09:28:36 +0000

    Add testing data for testapp

--- tests/testapp/models.py
+++ tests/testapp/models.py
@@ -4,43 +4,49 @@ from wagtail.fields import RichTextField
 from wagtail.models import Page
 from wagtail_vector_index.storage.models import (
     DefaultStorageVectorIndex,
+    EmbeddableFieldsVectorIndexMixin,
     EmbeddingField,
-    PageEmbeddableFieldsVectorIndexMixin,
     VectorIndexedMixin,
 )
 from wagtail_vector_index.storage.registry import registry
 
 
-class ExampleModel(VectorIndexedMixin, models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
+class MediaIndex(Page):
+    pass
+
+
+class Book(VectorIndexedMixin, Page):
+    body = RichTextField()
 
     embedding_fields = [EmbeddingField("title", important=True), EmbeddingField("body")]
+
+
+class Film(VectorIndexedMixin, Page):
+    description = models.TextField()
+
+    content_panels = [*Page.content_panels, FieldPanel("description")]
+
+    embedding_fields = [
+        EmbeddingField("title", important=True),
+        EmbeddingField("description"),
+    ]
+
+
+class VideoGame(VectorIndexedMixin, models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    embedding_fields = [
+        EmbeddingField("title", important=True),
+        EmbeddingField("description"),
+    ]
 
     def __str__(self):
         return self.title
 
 
-class ExamplePage(VectorIndexedMixin, Page):
-    body = RichTextField()
-
-    content_panels = [*Page.content_panels, FieldPanel("body")]
-
-    embedding_fields = [EmbeddingField("title", important=True), EmbeddingField("body")]
+class AllMediaVectorIndex(EmbeddableFieldsVectorIndexMixin, DefaultStorageVectorIndex):
+    querysets = [Book.objects.all(), Film.objects.all(), VideoGame.objects.all()]  # type: ignore
 
 
-class DifferentPage(VectorIndexedMixin, Page):
-    body = RichTextField()
-
-    content_panels = [*Page.content_panels, FieldPanel("body")]
-
-    embedding_fields = [EmbeddingField("title", important=True), EmbeddingField("body")]
-
-
-class MultiplePageVectorIndex(
-    PageEmbeddableFieldsVectorIndexMixin, DefaultStorageVectorIndex
-):
-    querysets = [ExamplePage.objects.all(), DifferentPage.objects.all()]  # type: ignore
-
-
-registry.register_index(MultiplePageVectorIndex())
+registry.register_index(AllMediaVectorIndex())
