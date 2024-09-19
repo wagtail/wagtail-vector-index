0b489a908cb49e5d24f68ee2a5722b5b7e26554b
Tom Usher <tom@tomusher.com>
2024-09-19 09:47:50 +0000

    Update tests to reference new example models

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,9 +1,9 @@
 import unittest
 
 import pytest
-from factories import DifferentPageFactory, ExamplePageFactory
+from factories import BookPageFactory, FilmPageFactory
 from faker import Faker
-from testapp.models import DifferentPage, ExamplePage
+from testapp.models import BookPage, FilmPage
 from wagtail_vector_index.ai_utils.backends.base import BaseEmbeddingBackend
 from wagtail_vector_index.storage import (
     registry,
@@ -41,9 +41,9 @@ def mock_embedding_backend():
 @pytest.fixture
 def test_pages():
     return [
-        ExamplePageFactory(title="Very similar to test"),
-        ExamplePageFactory(title="Somewhat similar to test"),
-        ExamplePageFactory(title="Not similar at all"),
+        BookPageFactory(title="Very similar to test"),
+        BookPageFactory(title="Somewhat similar to test"),
+        BookPageFactory(title="Not similar at all"),
     ]
 
 
@@ -67,7 +67,7 @@ def document_generator(test_pages):
 
 @pytest.fixture
 def mock_vector_index(mocker, mock_embedding_backend, document_generator):
-    vector_index = ExamplePage.vector_index
+    vector_index = BookPage.vector_index
 
     mock_backend = mock_embedding_backend(config=mocker.Mock())
     mocker.patch.object(
@@ -84,17 +84,17 @@ def mock_vector_index(mocker, mock_embedding_backend, document_generator):
 
 def test_registry():
     expected_class_names = [
-        "ExamplePageIndex",
-        "ExampleModelIndex",
-        "DifferentPageIndex",
-        "MultiplePageVectorIndex",
+        "BookPageIndex",
+        "FilmPageIndex",
+        "VideoGameIndex",
+        "AllMediaVectorIndex",
     ]
     assert set(registry._registry.keys()) == set(expected_class_names)
 
 
 def test_indexed_model_has_vector_index():
-    index = ExamplePage.vector_index
-    assert index.__class__.__name__ == "ExamplePageIndex"
+    index = BookPage.vector_index
+    assert index.__class__.__name__ == "BookPageIndex"
 
 
 def test_register_custom_vector_index():
@@ -105,9 +105,9 @@ def test_register_custom_vector_index():
 
 def test_get_embedding_fields_count(patch_embedding_fields):
     with patch_embedding_fields(
-        ExamplePage, [EmbeddingField("test"), EmbeddingField("another_test")]
+        BookPage, [EmbeddingField("test"), EmbeddingField("another_test")]
     ):
-        assert len(ExamplePage._get_embedding_fields()) == 2
+        assert len(BookPage._get_embedding_fields()) == 2
 
 
 def test_embedding_fields_override(patch_embedding_fields):
@@ -115,21 +115,21 @@ def test_embedding_fields_override(patch_embedding_fields):
     # multiple fields of the same type with the same name, only one
     # should be returned
     with patch_embedding_fields(
-        ExamplePage, [EmbeddingField("test"), EmbeddingField("test")]
+        BookPage, [EmbeddingField("test"), EmbeddingField("test")]
     ):
-        assert len(ExamplePage._get_embedding_fields()) == 1
+        assert len(BookPage._get_embedding_fields()) == 1
 
 
 def test_checking_search_fields_errors_with_invalid_field(patch_embedding_fields):
-    with patch_embedding_fields(ExamplePage, [EmbeddingField("foo")]):
-        errors = ExamplePage.check()
+    with patch_embedding_fields(BookPage, [EmbeddingField("foo")]):
+        errors = BookPage.check()
         assert "wagtailai.WA001" in [error.id for error in errors]
 
 
 @pytest.mark.django_db
 def test_index_get_documents_returns_at_least_one_document_per_page():
-    pages = ExamplePageFactory.create_batch(10)
-    index = registry["ExamplePageIndex"]
+    pages = BookPageFactory.create_batch(10)
+    index = registry["BookPageIndex"]
     index.rebuild_index()
     documents = index.get_documents()
     found_pages = {document.metadata.get("object_id") for document in documents}
@@ -139,34 +139,34 @@ def test_index_get_documents_returns_at_least_one_document_per_page():
 
 @pytest.mark.django_db
 def test_index_with_multiple_models():
-    example_pages = ExamplePageFactory.create_batch(5)
-    different_pages = DifferentPageFactory.create_batch(5)
-    index = registry["MultiplePageVectorIndex"]
+    book_pages = BookPageFactory.create_batch(5)
+    film_pages = FilmPageFactory.create_batch(5)
+    index = registry["AllMediaVectorIndex"]
     index.rebuild_index()
 
-    example_pages_ids = {str(page.pk) for page in example_pages}
-    different_page_ids = {str(page.pk) for page in different_pages}
+    book_pages_ids = {str(page.pk) for page in book_pages}
+    film_pages_ids = {str(page.pk) for page in film_pages}
     found_page_ids = {
         document.metadata["object_id"] for document in index.get_documents()
     }
 
-    assert found_page_ids == example_pages_ids.union(different_page_ids)
+    assert found_page_ids == book_pages_ids.union(film_pages_ids)
 
-    similar_result = list(index.find_similar(DifferentPage.objects.first()))
+    similar_result = list(index.find_similar(FilmPage.objects.first()))
     assert len(similar_result) > 1
     for p in similar_result:
-        assert isinstance(p, (ExamplePage, DifferentPage))
+        assert isinstance(p, (BookPage, FilmPage))
 
     search_result = list(index.search("test"))
     assert len(search_result) > 1
     for p in search_result:
-        assert isinstance(p, (ExamplePage, DifferentPage))
+        assert isinstance(p, (BookPage, FilmPage))
 
 
 @pytest.mark.django_db
 def test_similar_returns_no_duplicates(mocker):
-    pages = ExamplePageFactory.create_batch(10)
-    vector_index = ExamplePage.vector_index
+    pages = BookPageFactory.create_batch(10)
+    vector_index = BookPage.vector_index
 
     def gen_pages(cls, *args, **kwargs):
         yield from pages
@@ -189,8 +189,8 @@ def test_similar_returns_no_duplicates(mocker):
 
 @pytest.mark.django_db
 def test_query_passes_sources_to_backend(mocker):
-    ExamplePageFactory.create_batch(2)
-    index = ExamplePage.vector_index
+    BookPageFactory.create_batch(2)
+    index = BookPage.vector_index
     documents = index.get_documents()[:2]
 
     def get_similar_documents(query_embedding, limit=0, similarity_threshold=0.0):
@@ -207,8 +207,8 @@ def test_query_passes_sources_to_backend(mocker):
 
 @pytest.mark.django_db
 def test_query_with_similarity_threshold(mocker):
-    ExamplePageFactory.create_batch(2)
-    index = ExamplePage.vector_index
+    BookPageFactory.create_batch(2)
+    index = BookPage.vector_index
     documents = index.get_documents()[:2]
 
     def get_similar_documents(query_embedding, limit=0, similarity_threshold=0.5):
@@ -225,8 +225,8 @@ def test_query_with_similarity_threshold(mocker):
 
 @pytest.mark.django_db
 def test_find_similar_with_similarity_threshold(mocker):
-    pages = ExamplePageFactory.create_batch(10)
-    vector_index = ExamplePage.vector_index
+    pages = BookPageFactory.create_batch(10)
+    vector_index = BookPage.vector_index
 
     def gen_pages(cls, *args, **kwargs):
         yield from pages
--- tests/test_model_converter.py
+++ tests/test_model_converter.py
@@ -1,7 +1,7 @@
 import pytest
-from factories import DifferentPageFactory, ExampleModelFactory, ExamplePageFactory
+from factories import BookPageFactory, FilmPageFactory, VideoGameFactory
 from faker import Faker
-from testapp.models import ExamplePage
+from testapp.models import BookPage
 from wagtail_vector_index.ai import get_embedding_backend
 from wagtail_vector_index.storage.models import (
     EmbeddableFieldsDocumentConverter,
@@ -14,10 +14,10 @@ fake = Faker()
 @pytest.mark.django_db
 def test_get_split_content_adds_important_field_to_each_split(patch_embedding_fields):
     with patch_embedding_fields(
-        ExamplePage, [EmbeddingField("title", important=True), EmbeddingField("body")]
+        BookPage, [EmbeddingField("title", important=True), EmbeddingField("body")]
     ):
         body = fake.text(max_nb_chars=200)
-        instance = ExamplePageFactory.create(title="Important Title", body=body)
+        instance = BookPageFactory.create(title="Important Title", body=body)
         converter = EmbeddableFieldsDocumentConverter()
         splits = converter._get_split_content(instance, chunk_size=100)
         assert all(split.startswith(instance.title) for split in splits)
@@ -26,7 +26,7 @@ def test_get_split_content_adds_important_field_to_each_split(patch_embedding_fi
 @pytest.mark.django_db
 def test_convert_single_document_to_object():
     converter = EmbeddableFieldsDocumentConverter()
-    instance = ExamplePageFactory.create(
+    instance = BookPageFactory.create(
         title="Important Title", body=fake.text(max_nb_chars=200)
     )
     documents = list(
@@ -35,16 +35,16 @@ def test_convert_single_document_to_object():
         )
     )
     recovered_instance = converter.from_document(documents[0])
-    assert isinstance(recovered_instance, ExamplePage)
+    assert isinstance(recovered_instance, BookPage)
     assert recovered_instance.pk == instance.pk
 
 
 @pytest.mark.django_db
 def test_convert_multiple_documents_to_objects():
     converter = EmbeddableFieldsDocumentConverter()
-    example_objects = ExampleModelFactory.create_batch(5)
-    example_pages = ExamplePageFactory.create_batch(5)
-    different_pages = DifferentPageFactory.create_batch(5)
+    example_objects = VideoGameFactory.create_batch(5)
+    example_pages = BookPageFactory.create_batch(5)
+    different_pages = FilmPageFactory.create_batch(5)
     all_objects = list(example_objects + example_pages + different_pages)
     documents = converter.bulk_to_documents(
         all_objects, embedding_backend=get_embedding_backend("default")
